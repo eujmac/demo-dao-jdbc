2fa08710914733bb8c6e2f038b00cf73b08fa24a
João Marcos <joaomarcosjmac@gmail.com>
2023-01-16 08:59:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -11,8 +13,11 @@ public class Program {
 		Connection conn = DB.getConnection();
 		DB.closeConnection();
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
+		System.out.println(seller);
 	}
 
 }
