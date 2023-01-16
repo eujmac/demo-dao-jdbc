ee104719a8a8e1f09cdde6e35591deeab699d847
João Marcos <joaomarcosjmac@gmail.com>
2023-01-16 09:09:01 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.util.Date;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -18,6 +20,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
