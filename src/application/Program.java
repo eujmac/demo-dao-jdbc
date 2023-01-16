48f5ae2a5d0d285e14d8afc7448a03efc192d5e5
João Marcos <joaomarcosjmac@gmail.com>
2023-01-16 10:05:57 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,12 @@ public class Program {
 		for (Seller s : list) {
 			System.out.println(s);
 		}
+		
+		System.out.println("\n===Test 3: seller findByAll===");
+		list = sellerDao.findAll();
+		for (Seller s : list) {
+			System.out.println(s);
+		}
 	}
 
 }
